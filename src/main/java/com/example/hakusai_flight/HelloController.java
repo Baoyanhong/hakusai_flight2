540c52ffc76c062fba94498aadcc75755db3c91e
Bao Yanhong <yanhong@cac.co.jp>
2019-11-19 19:03:44 +0900

    change the page

--- src/main/java/com/example/hakusai_flight/HelloController.java
+++ src/main/java/com/example/hakusai_flight/HelloController.java
@@ -1,12 +1,20 @@
 package com.example.hakusai_flight;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
-@RequestMapping("/api")
+@Controller
+//@RequestMapping("/api")
 public class HelloController {
+
+	@GetMapping("/")
+	public String homePage(){
+		return "homepage";
+	}
+
+
 
 	@GetMapping("/byebye")
 	public String byebye() {
@@ -20,8 +28,7 @@ public class HelloController {
 	}
 
 	@GetMapping("/bye")
-
 	public String bye() {
-		return "Bye Spring Boot";
+		return "bye";
 	}
 }
